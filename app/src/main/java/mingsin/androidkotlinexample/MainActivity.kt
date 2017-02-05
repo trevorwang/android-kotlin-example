d1620b66340fa77b02960bd0742e704aada8d46a
Trevor Wang <oumei.vip@gmail.com>
2017-02-05 16:49:11 +0800

    integrate data binding

--- app/src/main/java/mingsin/androidkotlinexample/MainActivity.kt
+++ app/src/main/java/mingsin/androidkotlinexample/MainActivity.kt
@@ -1,5 +1,6 @@
 package mingsin.androidkotlinexample
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -7,12 +8,17 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import mingsin.androidkotlinexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.greating = "Hello Kotlin Data Binding!!!"
+
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
