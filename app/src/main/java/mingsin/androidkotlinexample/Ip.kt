d0922f69ea63b9c14b53a0ec64b1f3154232a1ed
Trevor Wang <oumei.vip@gmail.com>
2017-02-16 14:46:01 +0800

    integrate rxjava & retrofit

---
+++ app/src/main/java/mingsin/androidkotlinexample/Ip.kt
@@ -0,0 +1,6 @@
+package mingsin.androidkotlinexample
+
+/**
+ * Created by wangta on 2/16/17.
+ */
+data class Ip(val origin: String)
--- app/src/main/java/mingsin/androidkotlinexample/MainActivity.kt
+++ app/src/main/java/mingsin/androidkotlinexample/MainActivity.kt
@@ -6,11 +6,16 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
+import com.orhanobut.logger.Logger
 import mingsin.androidkotlinexample.databinding.ActivityMainBinding
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import rx.subscriptions.CompositeSubscription
 
 class MainActivity : AppCompatActivity() {
+    lateinit var apiService: ApiService
+
+    private val subscriptions = CompositeSubscription()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,25 +32,21 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+        apiService = RestClient().createApi()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+
+    override fun onStart() {
+        super.onStart()
+        subscriptions.add(apiService.ip().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Logger.d(it)
+                })
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
+    override fun onStop() {
+        super.onStop()
+        subscriptions.clear()
     }
 }
