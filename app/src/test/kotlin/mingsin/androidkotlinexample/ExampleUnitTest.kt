0108c0ebe802e515999c3aa9e130f2f8737cf2fa
Trevor Wang <trevor.wang@qq.com>
2017-12-19 23:08:45 +0800

    add robolectric to project

--- app/src/test/kotlin/mingsin/androidkotlinexample/ExampleUnitTest.kt
+++ app/src/test/kotlin/mingsin/androidkotlinexample/ExampleUnitTest.kt
@@ -3,12 +3,15 @@ package mingsin.androidkotlinexample
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
 
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
     @Test
     @Throws(Exception::class)
