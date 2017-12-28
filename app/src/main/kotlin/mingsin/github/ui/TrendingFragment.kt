a4c2a53a4e8b41b4dbae23a801ec8f9d9b02ed8e
Trevor Wang <trevor.wang@qq.com>
2017-12-28 21:56:54 +0800

    refactor trending adapter

--- app/src/main/kotlin/mingsin/github/ui/TrendingFragment.kt
+++ app/src/main/kotlin/mingsin/github/ui/TrendingFragment.kt
@@ -1,13 +1,8 @@
 package mingsin.github.ui
 
-import android.content.Context
-import android.content.Intent
 import android.databinding.DataBindingUtil
-import android.databinding.ViewDataBinding
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,10 +13,6 @@ import mingsin.github.R
 import mingsin.github.data.GithubApiService
 import mingsin.github.data.LanguageUtility
 import mingsin.github.databinding.FragmentTrendingBinding
-import mingsin.github.databinding.ItemRepoBinding
-import mingsin.github.databinding.RecyclerviewFooterBinding
-import mingsin.github.model.Repository
-import java.util.*
 import javax.inject.Inject
 
 /**
@@ -29,14 +20,14 @@ import javax.inject.Inject
  */
 class TrendingFragment : BaseFragment() {
     @Inject lateinit var api: GithubApiService
-    @Inject lateinit var lanUtils: LanguageUtility
+    @Inject lateinit var lanUtil: LanguageUtility
     lateinit var adapter: TrendingAdapter
     private lateinit var binding: FragmentTrendingBinding
 
     override fun onCreateContentView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_trending, container, false)
+        adapter = TrendingAdapter(context!!, lanUtil)
         binding.rvRepos.layoutManager = LinearLayoutManager(context)
-        adapter = TrendingAdapter(context!!, lanUtils)
         binding.rvRepos.adapter = adapter
         binding.rvRepos.addOnScrollListener(object : InfiniteScrollListener(10) {
             override fun loadMore(page: Int) {
@@ -53,7 +44,7 @@ class TrendingFragment : BaseFragment() {
     }
 
     private fun loadData(page: Int = 1) {
-        subscriptions.add(api.trending("created:>2016-12-17", page = page).subscribeOn(io()).observeOn(AndroidSchedulers.mainThread())
+        subscriptions.add(api.trending("created:>2017-12-27", page = page).subscribeOn(io()).observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     Logger.v("get repos %s", it)
                     adapter.repos += it.items
@@ -64,69 +55,4 @@ class TrendingFragment : BaseFragment() {
                     hideLoadingView()
                 })
     }
-
-
-    class TrendingAdapter(val context: Context, private val languageUtility: LanguageUtility) : RecyclerView.Adapter<ItemHolder<ViewDataBinding>>() {
-        private val itemTypeFooter = 0x100
-        var repos: List<Repository> = ArrayList()
-            set(value) {
-                val oldSize = field.size
-                field = value
-                if (oldSize < field.size) {
-                    notifyItemRangeChanged(oldSize, itemCount)
-                } else {
-                    notifyDataSetChanged()
-                }
-            }
-
-        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ItemHolder<ViewDataBinding> {
-            val inflater = LayoutInflater.from(context)
-            if (viewType == itemTypeFooter) {
-                val footerBinding = DataBindingUtil.inflate<RecyclerviewFooterBinding>(inflater, R.layout.recyclerview_footer, parent, false)
-                return ItemHolder(footerBinding)
-            }
-
-            val binding = DataBindingUtil.inflate<ItemRepoBinding>(inflater, R.layout.item_repo, parent, false)
-            return ItemHolder<ItemRepoBinding>(binding)
-        }
-
-
-        override fun onBindViewHolder(holder: ItemHolder<ViewDataBinding>?, position: Int) {
-            val binding = holder?.binding
-            when (binding) {
-                is ItemRepoBinding -> {
-                    val repo = repos[position]
-                    binding.repo = repo
-                    binding.lanUtility = languageUtility
-                    binding.root.setOnClickListener { v ->
-                        openProjectPage(repo)
-                    }
-                }
-                is RecyclerviewFooterBinding -> {
-
-                }
-            }
-        }
-
-        override fun getItemViewType(position: Int): Int {
-            if (repos.size == position) {
-                return itemTypeFooter
-            }
-            return super.getItemViewType(position)
-        }
-
-        private fun openProjectPage(repo: Repository) {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(repo.htmlUrl)
-            context.startActivity(intent)
-        }
-
-        override fun getItemCount(): Int {
-            if (repos.isEmpty()) {
-                return 0
-            }
-            return repos.count() + 1
-        }
-    }
-
 }
