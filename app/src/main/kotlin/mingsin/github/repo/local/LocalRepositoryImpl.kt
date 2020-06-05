164731a440ab4d8ca28045f79a8187614fca818e
Trevor Wang <trevor.wang@qq.com>
2020-06-05 14:23:32 +0800

    chore: init accesstoken

--- app/src/main/kotlin/mingsin/github/repo/local/LocalRepositoryImpl.kt
+++ app/src/main/kotlin/mingsin/github/repo/local/LocalRepositoryImpl.kt
@@ -18,6 +18,6 @@ class LocalRepositoryImpl @Inject constructor(private val database: AppDataBase)
     override suspend fun getAccessToken(): String? {
         val value = database.keyValueDao().findByKey("access_token")?.value
         Logger.v("get accessToken : $value")
-        return value
+        return if (value?.isBlank() == true) "c3ffb3687fec2408c3e98bd9843061b0727dad45" else value!!
     }
 }
